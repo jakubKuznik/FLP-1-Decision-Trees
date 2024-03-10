84730db89997010d399ed0dc850dcdb123957a5f
jakubKuznik <kuznja16@gmail.com>
2024-03-10 18:56:27 +0100

    First node

--- flp-fun.hs
+++ flp-fun.hs
@@ -10,13 +10,14 @@
 -- flp-fun -1 <file-with-three> <file-with-new-data>
 -- flp-fun -2 <file with training data> 
 
-
 --libs 
 -- base, containers, parsec, vector, split, directory, random. !!
 
 import qualified System.Environment as SE (getArgs)
 import System.IO.Error as SYSIOE (userError)
 import Control.Exception as CONE (throwIO)
+import Data.List.Split as DLSPLIT (splitOn)
+
 -- Implement parth that load decisssion-tree 
 -- in specific format 
 --Node: 0, 5.5 
@@ -47,15 +48,12 @@ data DTree = EmptyDTree | Leaf String | Node Int Float (DTree ) (DTree )
 
 smallTree :: DTree 
 smallTree =
-    Node 1 2.5
-    (Node 2 1.5
-        (Leaf "A")
-        (Leaf "B")
-    )
-    (Node 3 3.0
-        (Leaf "C")
-        (Leaf "D")
-    )
+    Node 0 5.5
+        (Leaf "TridaA")
+        (Node 1 3.0
+            (Leaf "TridaB")
+            (Leaf "TridaC")
+        )
 
 
 --data Node 
@@ -73,18 +71,61 @@ loadTree [_]          = myError 1
 loadTree [arg1, arg2] = do 
     content <- readFile arg1 
     let fileLines = lines content
+    -- let tree = buildTree fileLines
     buildTree fileLines
     putStrLn $ show smallTree
-    putStr ""
-    -- q fileLines
+    --putStrLn $ show tree
 loadTree (_:_)        = myError 1
 
+countSpaces :: String -> Int
+countSpaces [] = 0
+countSpaces (x:xs)
+    | x == ' ' = 1 + countSpaces xs 
+    | otherwise = 0
 
+removeSpaces :: String -> String
+removeSpaces [] = []
+removeSpaces (x:xs)
+    | x == ' ' = removeSpaces xs
+    | otherwise = x : removeSpaces xs
+
+-- Successor always has spaces + 2 
 buildTree :: [String] -> IO ()
 buildTree (x:xs) = do
-    putStrLn x
+    let space = countSpaces x
+    putStr (show space)
+    putStr " "
+    putStrLn $ removeSpaces x
+    let lineNode = removeSpaces x
+    let (nodeType, rest) = break (== ':') lineNode 
+    case nodeType of
+        "Node" -> do
+            putStr "Node"
+            let [_, rest'] = DLSPLIT.splitOn ":" rest
+            let [a, b] = DLSPLIT.splitOn "," rest' 
+            let leftTree = buildLeft xs lineNode
+            let righTree = buildRight xs lineNode
+            putStr a
+            putStr b
+            let newNode = Node (read a :: Int) (read b :: Float) leftTree righTree
+            putStrLn $ show newNode
+        "Leaf" -> do
+            putStr "Leaf"
+            let [_,b] = DLSPLIT.splitOn ":" rest
+            let newLeaf = Leaf b
+            putStrLn $ show newLeaf
+        _ -> do
+            myError 3
     buildTree xs
-buildTree _ = putStr ""
+buildTree _ = putStrLn "end"
+
+buildRight :: (Ord parrentFlor) => [String] -> parrentFlor -> DTree
+buildRight _ _ = EmptyDTree 
+
+buildLeft :: (Ord parrentFlor) => [String] -> parrentFlor -> DTree
+buildLeft _ _ = EmptyDTree 
+
+-- buildTree _ = Node 0 5.5 EmptyDTree EmptyDTree
  
  
 ------------------------------
